76dd88ae34b3935416f925b9787b9ef1d3a706f4
Jona Pierini <jonapierini@gmail.com>
2023-01-07 13:15:00 -0300

    agregado newProduct

--- Clase3.js
+++ Clase3.js
@@ -1,44 +1,38 @@
 class ProductManager {
   constructor() {
-    this.products = []
+    this.products = [];
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
-    if(this.products.find((prod) => prod.code === code)) return;
-    if(!title || !description || !thumbnail || !code || isNaN(price) || isNaN(stock) ) return
-    let id = 0
-     this.products.push(    
-         {
-            title, 
-            description,  
-            thumbnail, 
-            code,
-            price,
-            stock,
-         }
-     )
-     id = id +1
+    if (this.products.find((prod) => prod.code === code)) return;
+    if (!title || !description || !thumbnail ||!code ||isNaN(price) || isNaN(stock)) return;
+    let newProduct = {
+      title,
+      description,
+      thumbnail,
+      code,
+      price,
+      stock,
+      id: this.products.length + 1,
+    };
+    this.products.push(newProduct);
   }
 
   getProducts() {
-    return this.products
+    return this.products;
   }
 
-  getProductById (id) {
-    if(this.products.find((prod) => prod.id === id)) {
-        console.log('Producto Encontrado')
-    } else{
-        console.log('Not Found')
+  getProductById(id) {
+    if (this.products.find((prod) => prod.id === id)) {
+      console.log("Producto Encontrado");
+    } else {
+      console.log("Not Found");
     }
-    
   }
-
 }
 
-let nuevo1 = new ProductManager()
-nuevo1.addProduct('producto1', 'rico', 100.50, 100, 100, 100)
+let nuevo1 = new ProductManager();
+nuevo1.addProduct("producto1", "rico", 100.5, 100, 100, 100);
 nuevo1.getProducts()
-console.log(nuevo1)
-
 
 
