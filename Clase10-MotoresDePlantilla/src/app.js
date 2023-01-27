0a96738e05464580b6bfcdb620e8be795c8d1910
JonaPierini <jonapierini@gmail.com>
2023-01-27 14:29:21 -0300

    27.1.23.14.29

--- Clase10-MotoresDePlantilla/src/app.js
+++ Clase10-MotoresDePlantilla/src/app.js
@@ -1,16 +1,42 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
 
-app.engine('handlebars', exphbs.engine())
-app.set('views engine', '.handlebars')
 
-app.set('views', './views')
+app.engine("handlebars", exphbs.engine());
 
-app.get('/', (req, res)=> {
-    res.render('index', {})
+app.set("views engine", ".handlebars");
+app.set("views", "./views");
+
+let usuario = [
+  {
+    nombre: "Nombre 1",
+    apellido: "Apellido 1",
+    edad: 30,
+  },
+  {
+    nombre: "Nombre 2",
+    apellido: "Apellido 2",
+    edad: 40,
+  },
+  {
+    nombre: "Nombre 3",
+    apellido: "Apellido 3",
+    edad: 50,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { usuario });
+});
+
+app.get('/user/:id', (req, res)=>{
+    let usuario = usuarios[req.params.id]
+    res.render('user', usuario)
 })
 
-const PORT = 8080
-const server = app.listen(PORT, ()=> console.log('Server running on PORT 8000'))
-server.on('erro', error => console.log(error))
+const PORT = 8080;
+const server = app.listen(PORT, () =>
+  console.log("Server running on PORT 8000")
+);
+server.on("erro", (error) => console.log(error));
